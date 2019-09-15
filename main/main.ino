f494d6a4c735be6f6f1363692b15803b2ca70fd8
Ryan Stille <ryan@stillnet.org>
2019-09-15 16:30:53 -0500

    move letter index setup to code to helper method. Add 'ready' message upon startup.

--- main/main.ino
+++ main/main.ino
@@ -5,36 +5,12 @@
  
 CRGB leds[NUM_LEDS];
 
-int letter_positions[255];
+// could make this array smaller by storing an offset and only indexing up to 25.
+int letter_positions[122];
 
 void setup() { 
   // map letters to LEDs in the string of LEDs
-  letter_positions[97]  =  0;  // a
-  letter_positions[98]  =  1;  // b
-  letter_positions[99]  =  2;  // c
-  letter_positions[100] =  3;  // d
-  letter_positions[101] =  4;  // e
-  letter_positions[102] =  5;  // f
-  letter_positions[103] =  6;  // g
-  letter_positions[104] =  7;  // h
-  letter_positions[105] =  8;  // i
-  letter_positions[106] =  9;  // j
-  letter_positions[107] = 10;  // k
-  letter_positions[108] = 11;  // l
-  letter_positions[109] = 12;  // m
-  letter_positions[110] = 13;  // n
-  letter_positions[111] = 14;  // o
-  letter_positions[112] = 15;  // p
-  letter_positions[113] = 16;  // q
-  letter_positions[114] = 17;  // r
-  letter_positions[115] = 18;  // s
-  letter_positions[116] = 19;  // t
-  letter_positions[117] = 20;  // u
-  letter_positions[118] = 21;  // v
-  letter_positions[119] = 22;  // w
-  letter_positions[120] = 23;  // x
-  letter_positions[121] = 24;  // y
-  letter_positions[122] = 25;  // z
+  setupLetterPositions();
  
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
   Serial.begin(115200);
@@ -48,10 +24,11 @@ void setup() {
   Serial.print(F("Program started. Took "));
   Serial.print(sanityCheck);
   Serial.println(F(" counts for serial port to become ready."));
+
+  showReadyMessage();
  }
  
 void loop() {
-  int numArray[] = {3, 8, 4, 12, 17, 25};
   writeWord("hello");
   FastLED.clear();
   FastLED.show();
@@ -80,5 +57,45 @@ void fadeLED_on(int ledindex, int wait) {
     leds[ ledindex ].red = i;
     FastLED.show();
     delay(1);
+  }
+}
+
+void setupLetterPositions() {
+  letter_positions[97]  =  0;  // a
+  letter_positions[98]  =  1;  // b
+  letter_positions[99]  =  2;  // c
+  letter_positions[100] =  3;  // d
+  letter_positions[101] =  4;  // e
+  letter_positions[102] =  5;  // f
+  letter_positions[103] =  6;  // g
+  letter_positions[104] =  7;  // h
+  letter_positions[105] =  8;  // i
+  letter_positions[106] =  9;  // j
+  letter_positions[107] = 10;  // k
+  letter_positions[108] = 11;  // l
+  letter_positions[109] = 12;  // m
+  letter_positions[110] = 13;  // n
+  letter_positions[111] = 14;  // o
+  letter_positions[112] = 15;  // p
+  letter_positions[113] = 16;  // q
+  letter_positions[114] = 17;  // r
+  letter_positions[115] = 18;  // s
+  letter_positions[116] = 19;  // t
+  letter_positions[117] = 20;  // u
+  letter_positions[118] = 21;  // v
+  letter_positions[119] = 22;  // w
+  letter_positions[120] = 23;  // x
+  letter_positions[121] = 24;  // y
+  letter_positions[122] = 25;  // z
+}
+
+void showReadyMessage() {
+  char msg[] = {'r', 'e', 'a', 'd', 'y'};
+  for (int i=0; i<5; i++) {
+    FastLED.clear();
+    FastLED.show();
+    leds[ letter_positions[(int) msg[i]] ].green = 150;
+    FastLED.show();
+    delay(400);
   }
 }
